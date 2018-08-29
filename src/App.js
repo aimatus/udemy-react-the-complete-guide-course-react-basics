eb96c2e6e3a34ef12078683b87400f29f9788c2e
Abraham Matus <aimatusco@gmail.com>
2018-08-29 13:17:12 -0500

    Creates a basic component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
@@ -13,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Hi, I'm a React App!</p>
+        <Person />
       </div>
     );
   }
