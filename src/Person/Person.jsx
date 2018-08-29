51b0253cb9aecbfc9ba8c89c80f0a0d08f765a44
Abraham Matus <aimatusco@gmail.com>
2018-08-29 14:24:43 -0500

    Adds children property for hobbies.

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const person = (props) => {
-    return <p>I'm { props.name}  and I'm { props.age }</p>
+    return (
+        <div>
+            <p>I'm { props.name}  and I'm { props.age }</p>
+            <p>{ props.children }</p>
+        </div>
+    )
 }
 
 export default person;
