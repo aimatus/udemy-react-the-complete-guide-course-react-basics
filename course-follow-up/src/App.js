41ab376991608109a55d42239dfc61ee5360911c
Abraham Matus <aimatusco@gmail.com>
2018-09-02 16:08:44 -0500

    Updates Person components rendering conditional control.

--- course-follow-up/src/App.js
+++ course-follow-up/src/App.js
@@ -50,6 +50,28 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Ilych')}
+            changed={this.nameChangeHandler}>
+            My hobbies: playing guitar
+              </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      )
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -59,24 +81,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHalder}>Toggle Persons</button>
-        {this.state.showPersons ?
-          <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, 'Ilych')}
-              changed={this.nameChangeHandler}>
-              My hobbies: playing guitar
-              </Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-          </div>
-          : null
-        }
+        {persons}
       </div>
     );
   }
