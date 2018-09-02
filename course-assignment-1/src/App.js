e1678850b9027f65882edad392766e96fb99392e
Abraham Matus <aimatusco@gmail.com>
2018-09-02 09:34:37 -0500

    Adds event handler method.

--- course-assignment-1/src/App.js
+++ course-assignment-1/src/App.js
@@ -9,6 +9,12 @@ class App extends Component {
     username: '@aimatus'
   }
 
+  usernameChangeHandler = (event) => {
+    this.setState({
+        username: event.target.value
+      });
+  }
+
   render() {
     return (
       <div className="App">
