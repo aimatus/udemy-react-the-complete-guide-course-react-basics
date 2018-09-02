ba5f6b7fda458fe2872093e082525aad24307512
Abraham Matus <aimatusco@gmail.com>
2018-09-02 09:09:50 -0500

    Moves paragraphs content to const.

--- course-assignment-1/src/UserOutput/UserOutput.jsx
+++ course-assignment-1/src/UserOutput/UserOutput.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
-const userOutput = () => {
+const userOutput = (props) => {
+
+    const paragrahps = [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices lacinia dui, quis vestibulum est. Etiam feugiat mauris in dolor suscipit, sed faucibus enim aliquam. Etiam eu gravida lacus, sed condimentum enim. Sed et diam placerat, accumsan arcu vel, pulvinar massa.',
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque auctor lectus sed enim tempor ultricies. Maecenas semper elementum quam at rhoncus. Nulla consequat ac dolor id semper. Cras vitae nisi est. Pellentesque facilisis vulputate justo eu sollicitudin.'
+    ]
+
     return (
         <div>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices lacinia dui, quis vestibulum est. Etiam feugiat mauris in dolor suscipit, sed faucibus enim aliquam. Etiam eu gravida lacus, sed condimentum enim. Sed et diam placerat, accumsan arcu vel, pulvinar massa.
+                {paragrahps[0]}
             </p>
             <p>
-                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque auctor lectus sed enim tempor ultricies. Maecenas semper elementum quam at rhoncus. Nulla consequat ac dolor id semper. Cras vitae nisi est. Pellentesque facilisis vulputate justo eu sollicitudin.
+                {paragrahps[1]}
             </p>
         </div>
     )
