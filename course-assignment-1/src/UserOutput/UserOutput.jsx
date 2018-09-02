a9043bc630a598841a2758bc6ade1f573c303570
Abraham Matus <aimatusco@gmail.com>
2018-09-02 09:01:36 -0500

    Adds paragraphs to UserOutput and missing export default.

--- course-assignment-1/src/UserOutput/UserOutput.jsx
+++ course-assignment-1/src/UserOutput/UserOutput.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const userOutput = () => {
     return (
         <div>
-            <p />
-            <p />
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices lacinia dui, quis vestibulum est. Etiam feugiat mauris in dolor suscipit, sed faucibus enim aliquam. Etiam eu gravida lacus, sed condimentum enim. Sed et diam placerat, accumsan arcu vel, pulvinar massa.
+            </p>
+            <p>
+                Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque auctor lectus sed enim tempor ultricies. Maecenas semper elementum quam at rhoncus. Nulla consequat ac dolor id semper. Cras vitae nisi est. Pellentesque facilisis vulputate justo eu sollicitudin.
+            </p>
         </div>
     )
 }
+
+export default userOutput;
