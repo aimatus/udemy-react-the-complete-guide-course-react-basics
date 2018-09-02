41988840cb41679dcfea3419abda98534b0bd7a2
Abraham Matus <aimatusco@gmail.com>
2018-09-02 09:59:54 -0500

    Adds UserInput inline CSS styles.

--- course-assignment-1/src/UserInput/UserInput.jsx
+++ course-assignment-1/src/UserInput/UserInput.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
 const userInput = (props) => {
+
+    const containerCssStyles = {
+        textAlign: 'center'
+    }
+
+    const labelCssStyles = {
+        padding: '16px',
+        color: "#2E86C1"
+    }
+
+    const cssStyles = {
+        padding: '8px',
+        width: '300px',
+        color: '#2ECC71',
+        fontSize: '18px'
+    }
+
     return (
-        <input
-            type="text"
-            onChange={props.changed}
-            value={props.username} />
+        <div style={containerCssStyles}>
+            <label
+                style={labelCssStyles}>
+                Enter a username:
+            </label>
+            <input
+                type="text"
+                onChange={props.changed}
+                value={props.username}
+                style={cssStyles} />
+        </div>
     )
 }
 
