f96eec0602cb9d7f2a7aeeb6fc928778e810e8e1
Abraham Matus <aimatusco@gmail.com>
2018-09-02 13:02:13 -0500

    Customizes app title and content.

--- course-follow-up/src/App.js
+++ course-follow-up/src/App.js
@@ -54,11 +54,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React, the Complete Guide</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <button
           style={style}
           onClick={this.togglePersonsHalder}>Toggle Persons</button>
