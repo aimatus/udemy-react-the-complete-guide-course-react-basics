be367bfc90b956ce0f1af2aba83131d47ff70c4b
Abraham Matus <aimatusco@gmail.com>
2018-10-16 21:18:16 -0500

    [Burger Builder] Adds BurgerBuilder component and CSS styles

---
+++ burger-builder-app/src/components/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BurgerBuilder extends React.Component {
+    render() {
+        return (
+            <>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </>
+        );
+    }
+}
+
+export default BurgerBuilder;
--- burger-builder-app/src/components/Layout/Layout.jsx
+++ burger-builder-app/src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <>
         <div>
             Toolbar, SideBar, BackDrop
         </div>
-        <main>
-            { props.children }
+        <main className={classes.Content}>
+            {props.children}
         </main>
     </>
 );
