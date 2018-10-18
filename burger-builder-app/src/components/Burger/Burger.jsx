e076f9d268b8a646e5dc50b67e5d9ae972a586be
Abraham Matus <aimatusco@gmail.com>
2018-10-18 17:20:09 -0500

    [Burger Builder] Generates Burger ingredients dynamically

--- burger-builder-app/src/components/Burger/Burger.jsx
+++ burger-builder-app/src/components/Burger/Burger.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-import BurgerIngredient  from './BurgerIngredient/BurgerIngredient'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.module.css'
 
 const burger = (props) => {
+
+    const ingredientsList = Object.keys(props.ingredients)
+        .map(ingredientKey => {
+            return [...Array(props.ingredients[ingredientKey])]
+                .map((_, index) => {
+                    return <BurgerIngredient
+                        key={ingredientKey + index}
+                        type={ingredientKey} />
+                });
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={"bread-top"} />
-            <BurgerIngredient type={"cheese"} />
-            <BurgerIngredient type={"meat"} />
+            {ingredientsList}
             <BurgerIngredient type={"bread-bottom"} />
         </div>
     );
