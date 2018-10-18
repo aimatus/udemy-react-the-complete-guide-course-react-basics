30b3fd38247adbc0c1600fc26248922c633d8015
Abraham Matus <aimatusco@gmail.com>
2018-10-17 22:11:21 -0500

    Adds Burger component to app

--- burger-builder-app/src/components/BurgerBuilder/BurgerBuilder.jsx
+++ burger-builder-app/src/components/BurgerBuilder/BurgerBuilder.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Burger from '../Burger/Burger'
 
 class BurgerBuilder extends React.Component {
     render() {
         return (
             <>
-                <div>Burger</div>
+                <Burger />
                 <div>Build Controls</div>
             </>
         );
