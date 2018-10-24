ae7e8b6c8f3265d82eb94105b3e0c1a3dc45afe3
Abraham Matus <aimatusco@gmail.com>
2018-10-23 21:38:48 -0500

    [Burger Builder] Adds Toolbar component

--- burger-builder-app/src/components/Layout/Layout.jsx
+++ burger-builder-app/src/components/Layout/Layout.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <>
-        <div>
-            Toolbar, SideBar, BackDrop
-        </div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
