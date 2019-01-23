2c63a4a1d44a334a6bc40016bdce06fed111b55a
Abraham Matus <aimatusco@gmail.com>
2019-01-22 21:07:08 -0500

    [Redux] Adds dispatching actions

--- redux/redux-basics.js
+++ redux/redux-basics.js
@@ -7,6 +7,17 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    } else if (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
     return state;
 };
 
@@ -15,5 +26,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
 
 // Subscription
