412df625a1166dfa14c7ee6a33173a7fabda16f9
Abraham Matus <aimatusco@gmail.com>
2019-01-31 19:56:23 -0500

    [Burger Builder] Changes Modal and OrderSummary components to statefull ones and re-renders only when proper state changes

--- burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.jsx
+++ burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients)
+class OrderSummary extends React.Component {
+    render() {
+
+        const ingredientsSummary = Object.keys(this.props.ingredients)
         .map(ingredientKey => (
             <li key={ingredientKey}>
                 <span style={{ textTransform: 'capitalize' }}>{ingredientKey}: </span>
-                {props.ingredients[ingredientKey]}
+                {this.props.ingredients[ingredientKey]}
             </li>
         ));
 
-    return (
-        <>
-            <h3>Your Order</h3>
-            <p>A delicious burger with this ingredients:</p>
-            <ul>{ingredientsSummary}</ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button
-                buttonType='Danger'
-                clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button
-                buttonType='Success'
-                clicked={props.purchaseContinued}>CONTINUE</Button>
-        </>
-    );
+        return (
+            <>
+                <h3>Your Order</h3>
+                <p>A delicious burger with this ingredients:</p>
+                <ul>{ingredientsSummary}</ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button
+                    buttonType='Danger'
+                    clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button
+                    buttonType='Success'
+                    clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- burger-builder-app/src/components/UI/Modal/Modal.jsx
+++ burger-builder-app/src/components/UI/Modal/Modal.jsx
@@ -2,24 +2,31 @@ import React from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => {
-    const modalDynamicStyle = {
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-    };
+class Modal extends React.render {
+    
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-    return (
-        <>
-            <Backdrop
-                show={props.show}
-                clicked={props.modalClosed} />
-            <div
-                className={classes.Modal}
-                style={modalDynamicStyle}>
-                {props.children}
-            </div>
-        </>
-    );
+    render() {
+        const modalDynamicStyle = {
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+        };
+
+        return (
+            <>
+                <Backdrop
+                    show={this.props.show}
+                    clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={modalDynamicStyle}>
+                    {this.props.children}
+                </div>
+            </>
+        )
+    }
 };
 
-export default modal;
+export default Modal;
