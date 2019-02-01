1275d1da751fd79180118e8aa40f19b040128c04
Abraham Matus <aimatusco@gmail.com>
2019-02-01 10:55:04 -0500

    [HTTP Ajax] Consumes posts from endpoint and shows the title in the application

--- reaching-out-to-the-web-http-ajax/src/containers/Blog/Blog.js
+++ reaching-out-to-the-web-http-ajax/src/containers/Blog/Blog.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({ posts: response.data });
+            });
+    }
+
     render () {
+        const posts = this.state.posts.map(post =>
+            <Post key={post.id} title={post.title}/>
+        );
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
