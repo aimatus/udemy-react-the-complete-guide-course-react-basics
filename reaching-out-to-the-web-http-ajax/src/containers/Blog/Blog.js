3c37286dde6f1a3a01a3d46bf15387a0f33ac8fa
Abraham Matus <aimatusco@gmail.com>
2019-03-25 17:28:13 -0500

    [HTTP Ajax] Makes blog clickable

--- reaching-out-to-the-web-http-ajax/src/containers/Blog/Blog.js
+++ reaching-out-to-the-web-http-ajax/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -25,9 +26,16 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
     render () {
         const posts = this.state.posts.map(post =>
-            <Post key={post.id} title={post.title} author={post.author}/>
+            <Post key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}/>
         );
 
         return (
@@ -36,7 +44,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
