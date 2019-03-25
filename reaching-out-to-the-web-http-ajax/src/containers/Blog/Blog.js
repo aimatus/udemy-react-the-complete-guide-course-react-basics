ba97573279c226f6ed310b0ec451997773bbb12e
Abraham Matus <aimatusco@gmail.com>
2019-03-25 17:17:53 -0500

    [HTTP Ajax] Limits posts to 4

--- reaching-out-to-the-web-http-ajax/src/containers/Blog/Blog.js
+++ reaching-out-to-the-web-http-ajax/src/containers/Blog/Blog.js
@@ -14,13 +14,20 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({ posts: response.data });
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Abe'
+                    }
+                });
+                this.setState({ posts: updatedPosts });
             });
     }
 
     render () {
         const posts = this.state.posts.map(post =>
-            <Post key={post.id} title={post.title}/>
+            <Post key={post.id} title={post.title} author={post.author}/>
         );
 
         return (
